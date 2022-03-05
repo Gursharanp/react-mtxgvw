3061847a2a9162b21df94c4fc6deaadfeeae4288
Gursharanp <gursharanpreetsinghkang2@gmail.com>
2022-03-05 12:22:36 +0530

    1 file modified

--- src/NewMe.js
+++ src/NewMe.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { pure } from 'recompose';
+import React,{memo} from 'react';
+// import { pure } from 'recompose';
 
 function NewMY(){
   console.log("child render")
   return <div> </div>
 };
 
-export default pure (NewMY);
+export default memo (NewMY);
